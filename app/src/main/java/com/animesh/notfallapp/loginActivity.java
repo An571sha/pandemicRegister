1ba56e244d9a5ff6d5a2040f7fdfe100ab686c85
An571sha <anim_esh@yahoo.co.in>
2020-06-11 12:48:36 +0200

    added all files

---
+++ app/src/main/java/com/animesh/notfallapp/loginActivity.java
@@ -0,0 +1,21 @@
+package com.animesh.notfallapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signup(View view) {
+    }
+
+    public void signin(View view) {
+    }
+}
---
+++ app/src/main/java/com/animesh/notfallapp/signupActivity.java
@@ -0,0 +1,18 @@
+package com.animesh.notfallapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class signupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void signup(View view) {
+    }
+}
